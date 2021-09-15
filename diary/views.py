076ec3e3f35c8ecd28120dcc29b2fe2ce609664a
odd-star <ksw2070134@stu.o-hara.ac.jp>
2021-09-15 20:03:01 +0900

    初期セットアップ(part10)

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,6 @@
+from django.contrib.messages.api import success
+from django.views.generic.edit import UpdateView
+import diary
 import logging
 from django.contrib.auth.models import User
 
@@ -8,9 +11,13 @@ from django.contrib import messages
 from.forms import InquiryForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from diary import forms
+
 logger = logging.getLogger(__name__)
 
 from .models import Diary
+
+from .forms import InquiryForm, DiaryCreateForm
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -29,6 +36,22 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name ='diary_detail.html'
 
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+class DiaryUpdateView(LoginRequiredMixin,generic.UpdateView):
+    model = Diary
+    template_name ='diary_update.html'
+    form_class = DiaryCreateForm
+
+class DiaryDeleteView(LoginRequiredMixin,generic.DeleteView):
+    model = Diary
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('diary:diary_list')
+
     def form_valid(self, form): 
         form.send_email()
         messages.success(self.request,'メッセージを送信しました。')
@@ -38,3 +61,29 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request,'日記を作成しました。')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request,"日記の作成に失敗しました。")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('diary:diary_detail',kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request,"日記を更新しました。")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の更新に失敗しました。")
+        return super().form_invalid(form)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self, request, "日記を削除しました。")
+        return super().delete(request, *args, **kwargs)
